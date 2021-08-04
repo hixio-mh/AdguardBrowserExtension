69c17520fb2f1b774d8907ae2a38b926ada08692
Slava Leleka <v.leleka@adguard.com>
2021-08-04 12:12:17 +0300

    AG-9471 allow search ads by default
    Merge in EXTENSIONS/browser-extension from fix/AG-9471 to feature/AG-2737
    
    Squashed commit of the following:
    
    commit cf69a724ba2714ef1f0e9398f58ec5e2f9fe5998
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Tue Aug 3 22:43:28 2021 +0300
    
        allow search ads by default

--- Extension/src/background/settings/default-settings.js
+++ Extension/src/background/settings/default-settings.js
@@ -5,7 +5,7 @@ import { settings } from './user-settings';
  */
 export const defaultSettings = {
     'general-settings': {
-        'allow-acceptable-ads': false,
+        'allow-acceptable-ads': true,
         'show-blocked-ads-count': !settings.defaultProperties[settings.DISABLE_SHOW_PAGE_STATS],
         'autodetect-filters': !settings.defaultProperties[settings.DISABLE_DETECT_FILTERS],
         'safebrowsing-enabled': !settings.defaultProperties[settings.DISABLE_SAFEBROWSING],
@@ -22,10 +22,12 @@ export const defaultSettings = {
     'filters': {
         'enabled-groups': [
             1,
+            6,
             7,
         ],
         'enabled-filters': [
             2,
+            10,
         ],
         'custom-filters': [],
         'user-filter': {
